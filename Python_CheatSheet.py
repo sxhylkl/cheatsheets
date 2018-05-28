99ac9acab45ab691686731bf476c7e739aa6da7a
frankly <39119092+franklyfree@users.noreply.github.com>
2018-05-28 15:45:18 +0200

    Update Python_CheatSheet.py

--- Python_CheatSheet.py
+++ Python_CheatSheet.py
@@ -1,40 +1,81 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-#nach attribut name in liste l suchen
-any(x.name == "t2" for x in l)
+### debugging ###
+import pdb; pdb.set_trace()
 
-#===============================================================================
-# >>> from timeit import timeit
-# >>> timeit("[]")
-#===============================================================================
+
+### search for values of attributes inside list lst ###
+any(obj.name == 'value' for obj in lst)
+
+
+### performance tests ###
+from timeit import timeit
+timeit(lambda: <function>(<argument>))
+
 
 ### dynamic strings ###
 print("Mein Name ist %s, Nachname %s" % ('Faruk', 'Heynemann'))
 
-# f-strings since Python 3.6.3
 name = 'Faruk'
-print(f'Hi {name} how are you doing?')
+print(f'Hi {name} how are you doing?')  # f-strings since Python 3.6
+
+
+### String conversion ###
+b"abcde".decode("utf-8")
+"abcde".encode("utf-8")
+
+
+### Get size of builtin objects ###
+getsizeof()
+
+
+### calculate times ###
+from datetime import timedelta
+print(timedelta(hours=15, minutes=10, seconds=29) - timedelta(hours=14, minutes=59, seconds=55))
 
 
 ### ternäry operator ###
 condition = True
 a if condition else b  # funktioniert auch mit defs
-
-# Hässlichere varianten:
+# Ugly:
 # (if C: x else: y)  # Eclipse mag das nicht
 # (falseValue, trueValue)[test]
 # (falseValue, trueValue)[test == True]
 # (falseValue, trueValue)[bool(condition)]
 # result = {True: x, False: y}[a > b]
 # (falseValue, trueValue)[test]
-### Tern�hrer Operator ###
+### Ternary operator ###
 # https://en.wikipedia.org/wiki/%3F:#Python
 
-getsizeof()
 
-b"abcde".decode("utf-8")
-"abcde".encode("utf-8")
+### Properties ###
+# variant 1 #
+_x = None
+@property
+def x(self): return self._x
+@x.setter
+def x(self, value): self._x = value
+@x.deleter
+def x(self): del self._x
+
+# variant 2 #
+_y = None
+def y_get(): return _y
+def y_set(value): _y = value
+def y_del(): del _y
+y = property(fget=y_get, fset=y_set, fdel=x_del, doc=None)
+
+# variant 3 #
+_z = None
+def z_get(): return _z
+def z_set(value): _z = value
+def z_del(): del _z
+z = property()
+z = z.getter(z_get)
+z = z.setter(z_set)
+z = z.delete(z_del)
+
 
 
 def list_compare(self, old, new):
@@ -62,68 +103,12 @@ def get_x_by_filter(list_, filter_='', keep=-1, newest=True):
     return ret
 
 
-import pdb
-pdb.set_trace()
-
-import time
-start_time = time.time()
-main()
-print("--- %s seconds ---" % (time.time() - start_time))
-
-class timing:
-
-    def __init__(self):
-        import atexit
-        from time import time, strftime, localtime
-        from datetime import timedelta
-        start = time()
-        atexit.register(endlog)
-        log("Start Program")
-
-    def secondsToStr(elapsed=None):
-        if elapsed is None:
-            return strftime("%Y-%m-%d %H:%M:%S", localtime())
-        else:
-            return str(timedelta(seconds=elapsed))
-    
-    def log(s="", elapsed=None):
-        line = "="*40
-        print(line)
-        print(secondsToStr(), '-', s)
-        if elapsed:
-            print("Elapsed time:", elapsed)
-        print(line)
-        print()
-    
-    def endlog():
-        end = time()
-        elapsed = end - start
-        log("End Program", secondsToStr(elapsed))
-
-
-class C:
-
-    def __init__(self):
-        self._x = None
-
-    @property
-    def x(self):
-        """I'm the 'x' property."""
-        return self._x
-
-    @x.setter
-    def x(self, value):
-        self._x = value
-
-    @x.deleter
-    def x(self):
-        del self._x
-
-
 if __name__ == '__main__':
     pass
 
-""" What is false?
+
+### Truth / False List ###
+"""
 None
 False
 zero of any numeric type, for example, 0, 0L, 0.0, 0j.
@@ -133,5 +118,3 @@ instances of user-defined classes, if the class defines a __nonzero__() or
 __len__() method, when that method returns the integer zero or bool value False.
 """
 
- from datetime import timedelta
->>> print(timedelta(hours=15, minutes=10, seconds=29) - timedelta(hours=14, minutes=59, seconds=55))
